b873f0f0727aaebee69314bb3c39d5700cdd3291
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-03-01 23:26:18 +0530

    refresh page problem solved

--- src/App.js
+++ src/App.js
@@ -14,12 +14,8 @@ import Seller_login from './pages/Seller_login';
 import Sellers_material from './pages/Sellers_material'
 import Category from './pages/Category'
 import Seller_registration from './pages/Seller_registration';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';  // Use Routes instead of Switch (React Router v6)
+
 import Seller_account from './pages/Seller_account';
 import Seller_details from './pages/Seller_details';
 import Contact_us from './pages/Contact_us';
@@ -35,31 +31,29 @@ function App() {
     <div>
       <>
         {/* This is the alias of BrowserRouter i.e. Router */}
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/Login" element={<Login />} />
-            <Route exact path="/Contact" element={<Contact_us />} />
-            <Route exact path="/About" element={<About_us />} />
-            <Route exact path="/Signup" element={<Signup />} />
-            <Route exact path="/Categories" element={<Category />} />
-            <Route exact path="/User_account" element={<User_account />} />
-            <Route exact path="/Material_details/:id" element={<Material_details />} />
-            <Route exact path="/ForgotPassword" element={<ForgotPassword />} />
-            <Route exact path="/Sellers" element={<Sellers />} />
-            {/* <Route exact path="/SellersHome" element={<Sellers_home_scr />} /> */}
-            <Route exact path="/Products" element={<Product_list />} />
-            <Route exact path="/Search" element={<Search />} />
-            <Route exact path="/Result" element={<Result />} />
-            <Route exact path="/Category_material/:category" element={<Category_material />} />
-            <Route exact path="/Seller_login" element={<Seller_login />} />
-            <Route exact path="/Sellers_material/:seller_id" element={<Sellers_material />} />
-            <Route exact path="/Seller_registration" element={<Seller_registration />} />
-            <Route exact path="/Seller_details/:seller_id" element={<Seller_details />} />
-            <Route exact path="/Seller_account" element={<Seller_account />} />
-            <Route exact path="/Material_reg" element={<Material_reg />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Login" element={<Login />} />
+          <Route exact path="/Contact" element={<Contact_us />} />
+          <Route exact path="/About" element={<About_us />} />
+          <Route exact path="/Signup" element={<Signup />} />
+          <Route exact path="/Categories" element={<Category />} />
+          <Route exact path="/User_account" element={<User_account />} />
+          <Route exact path="/Material_details/:id" element={<Material_details />} />
+          <Route exact path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route exact path="/Sellers" element={<Sellers />} />
+          {/* <Route exact path="/SellersHome" element={<Sellers_home_scr />} /> */}
+          <Route exact path="/Products" element={<Product_list />} />
+          <Route exact path="/Search" element={<Search />} />
+          <Route exact path="/Result" element={<Result />} />
+          <Route exact path="/Category_material/:category" element={<Category_material />} />
+          <Route exact path="/Seller_login" element={<Seller_login />} />
+          <Route exact path="/Sellers_material/:seller_id" element={<Sellers_material />} />
+          <Route exact path="/Seller_registration" element={<Seller_registration />} />
+          <Route exact path="/Seller_details/:seller_id" element={<Seller_details />} />
+          <Route exact path="/Seller_account" element={<Seller_account />} />
+          <Route exact path="/Material_reg" element={<Material_reg />} />
+        </Routes>
 
         {/* <Home /> */}
       </>
