259f5b63d9f94f76b0f572942b8ecafb15308c65
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-05-03 22:24:06 +0530

    Update Sellers_home_scr.js

--- src/components/Sellers_home_scr.js
+++ src/components/Sellers_home_scr.js
@@ -52,8 +52,7 @@ function Sellers_home_scr() {
                     <ul className="nav nav-tabs links" id="myTab" role="tablist">
                         <li className="nav-item" role="presentation">
                             <Link className="btn btn-primary btn-sm" to="/Sellers">
-                                <i className="fa-solid fa-angle-right"></i>
-                            </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" /></svg>                            </Link>
                         </li>
                     </ul>
                 </div>
