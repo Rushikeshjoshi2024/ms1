aad33d0b2fbe236dc636498979115c7d3f1572a7
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-05-03 22:39:13 +0530

    added cdn link

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';  // Import HashRouter
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
