64125a6649073d28641306d74b39da25b0c2534f
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-07-31 23:52:19 +0530

    oauth2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';  // Import HashRouter
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const googleClientId = "952339138614-n4jja1bk5mc9mksahc093ogq33ggp1ac.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>  {/* Wrap your App component with HashRouter */}
-      <App />
-    </HashRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+
+      <HashRouter>  {/* Wrap your App component with HashRouter */}
+        <App />
+      </HashRouter>
+    </GoogleOAuthProvider>
+
   </React.StrictMode>
+
 );
 
 // For performance measuring (optional)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
 
 // import Nav from "../components/Nav";
+
+
+const [user, setUser] = useState(null);
+
+const handleLoginSuccess = async (credentialResponse) => {
+    console.log("Google Login Success:", credentialResponse);
+    const token = credentialResponse.credential;
+
+    try {
+        // Send the token to the backend for verification
+        const serverResponse = await axios.post(
+            `http://localhost:8080/api/auth/google`,
+            { token }
+        );
+
+        console.log("Server Response:", serverResponse.data);
+        // Set the user state with the data from the backend
+        setUser(serverResponse.data.user);
+
+    } catch (error) {
+        console.error("Login Failed:", error);
+    }
+};
+
+const handleLoginError = () => {
+    console.log('Login Failed');
+};
+
+const handleLogout = () => {
+    setUser(null);
+};
 
 function Login() {
     // const [name, setName] = useState('');
@@ -83,6 +115,10 @@ function Login() {
                                 </form>
                                 <div className="other-links">
                                     {/* <div className="text">Or login with</div> */}
+                                    <GoogleLogin
+                                        onSuccess={handleLoginSuccess}
+                                        onError={handleLoginError}
+                                    />
                                     <a href="'"><i className="fab fa-facebook-f"></i>Facebook</a><a href="/"><i className="fab fa-google"></i>Google</a><a href="/"><i className="fab fa-linkedin-in"></i>Linkedin</a>
                                 </div>
                                 <div className="page-links">
