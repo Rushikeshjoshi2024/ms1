cbc5ab93e034256e38b3bdf900b551c59f25a8bc
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-08-23 18:10:32 +0530

    Update Sellers_home_scr.js

--- src/components/Sellers_home_scr.js
+++ src/components/Sellers_home_scr.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './Sellers.css';
+import './component.css';
 
 // --- Helper Component: Star Rating ---
 const StarRating = ({ rating }) => {
